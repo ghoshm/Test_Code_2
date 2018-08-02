db0553432064033300990e6e635641b218b9ddbb
Marcus <ghoshm@hotmail.co.uk>
2018-08-02 14:04:28 +0100

    Thesis

--- mRMR_Errors.m
+++ mRMR_Errors.m
@@ -21,7 +21,7 @@ null_m(1,1) = p*100;
 null_m(1,2) = sqrt((p*(1-p))/n)*100; 
 
 %% 
-histcounts(mRMR_tw{er,1})
+histcounts(mRMR_tw{er,1});
 
 n = 28; 
 p = 9/n; 
@@ -31,10 +31,10 @@ null_m(1,1) = p*100;
 null_m(1,2) = sqrt((p*(1-p))/n)*100; 
 
 %% Figure 
-load('D:\Behaviour\SleepWake\Re_Runs\Threading\Draft_1\mRMR_Comparisons\table.mat'); 
+load('C:\Users\Marcus\Documents\Thesis\Tables\Mdl_Loss_data.mat');
 load('D:\Behaviour\SleepWake\Re_Runs\Threading\Draft_1\Post_Bout_Transitions.mat', 'cmap'); 
 load('D:\Behaviour\SleepWake\Re_Runs\Threading\Draft_1\Post_Bout_Transitions.mat', 'cmap_2');
-cmap{4} = [cmap{4}(2,:) ; cmap{4}(3,:) ; [1 0.5 0]] ;
+cmap{4} = [cmap{4}(2,:) ; cmap{4}(3,:) ; [1 0.5 0]]; % het, hom, orange 
 
 % colormap 
 n = 24; % number of colours 
@@ -45,29 +45,54 @@ CT = [linspace(cmap_2{1,1}(1,1),cmap_2{1,1}(2,1),n)'...
 cmap = [cmap{1} ; cmap{1} ; cmap{1} ; CT ; cmap{1} ; cmap{1};...  
     cmap{4}; cmap{4} ; cmap{4} ; cmap{6} ; cmap{7}]; 
 
-% data 
-clf; hold on; 
+%% WT Figure 
+figure; hold on; 
 set(gca,'FontName','Calibri'); box off; set(gca,'Layer','top'); set(gca,'Fontsize',32);
-errorbar(data(:,3),data(:,4),'o','color',([1 1 1]*(1-(1/(9)^.5))),'linewidth',3); 
-for i = 1:size(data,1)
+errorbar(data(1:29,3),data(1:29,4),'o','color',([1 1 1]*(1-(1/(9)^.5))),'linewidth',3); 
+for i = 1:29
+    errorbar(i,data(i,1),data(i,2),'color',cmap(i,:),...
+        'marker','o','linewidth',3);
+end
+
+% Label Line 
+plot([1 29],[55 55],'color',cmap_2{1}(2,:),'linewidth',3); 
+text(1,57,'Wild Type','FontSize',32,'FontName','Calibri');
+
+% Tags 
+set(gca,'XTick',1:29); 
+set(gca,'XTickLabels',MdlLossTags(1:29,1));
+xtickangle(45);
+
+% Nice Figure 
+axis([0 30 0 60]); 
+xlabel('Classifier','Fontsize',32); 
+ylabel('Classification Error (%)','Fontsize',32);
+
+%% Conditions Figure 
+figure; hold on; 
+set(gca,'FontName','Calibri'); box off; set(gca,'Layer','top'); set(gca,'Fontsize',32);
+errorbar(30:length(data),data(30:end,3),data(30:end,4),'o','color',([1 1 1]*(1-(1/(9)^.5))),'linewidth',3); 
+for i = 30:length(data)
     
     errorbar(i,data(i,1),data(i,2),'color',cmap(i,:),...
         'marker','o','linewidth',3);
 end
 
-% lines 
-plot([1 29],[55 55],'color',cmap_2{1}(2,:),'linewidth',3); 
+% Label Line 
 plot([30 38],[55 55],'color',[1 0.5 0],'linewidth',3); 
 plot([39 45],[55 55],'color',cmap(39,:),'linewidth',3); 
 plot([46 49],[55 55],'color',cmap(end,:),'linewidth',3); 
 
-% labels 
-text(12,57,'Wild Type','FontSize',32,'FontName','Calibri')
-text(32,57,'HcrtR','FontSize',32,'FontName','Calibri')
+text(30,57,'HcrtR','FontSize',32,'FontName','Calibri')
 text(39,57,'Melatonin','FontSize',32,'FontName','Calibri')
-text(46.5,57,'PTZ','FontSize',32,'FontName','Calibri')
+text(46,57,'PTZ','FontSize',32,'FontName','Calibri')
 
-axis([0 size(data,1)+1 0 60]); 
-set(gca,'XTick',[]);
+% Tags 
+set(gca,'XTick',30:length(data)); 
+set(gca,'XTickLabels',MdlLossTags(30:end,1));
+xtickangle(45);
+
+% Nice Figure 
+axis([29 50 0 60]); 
 xlabel('Classifier','Fontsize',32); 
 ylabel('Classification Error (%)','Fontsize',32);
