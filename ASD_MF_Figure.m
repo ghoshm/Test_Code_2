3da3b852e78de2124fc757464ccaad417c580adc
Marcus <ghoshm@hotmail.co.uk>
2018-08-23 21:20:37 +0100

    Thesis

--- ASD_MF_Figure.m
+++ ASD_MF_Figure.m
@@ -14,14 +14,14 @@ load('D:\Behaviour\SleepWake\Re_Runs\Threading\Thesis\180731.mat', 'night_color'
 er = 1; % set experiment of interest
 set_token = find(experiment_reps == er,1,'first'); % used for each experiments sets settings
 time_bins = fps{set_token}*60*15; % set smoothing window 
-m = [2 3]; % modules of interest (including inactive and active!).  
+m = [2 4]; % modules of interest (including inactive and active!).  
 
 % Smooth Data 
 for c = 1:length(m) % for each module of interest 
         clear data;
         
         data = states{er,1}(:,...
-            lb_merge{er,1}(5):lb_merge{er,1}(7)) == m(c);
+            lb_merge{er,1}(5):lb_merge{er,1}(7)) == m(c); % fish x time of interest 
         
         tic 
         for g = 1:max(i_group_tags(i_experiment_reps == er)) % for each fish
@@ -33,21 +33,20 @@ for c = 1:length(m) % for each module of interest
 end
 
 %% Cluster Frequencies Figure
-% Run one er @ a time to avoid low level graphics errors
 
-figure; % make a figure for this experiment
+figure; 
 for c = 1:length(m) % for each module of interest 
-    subplot(1,2,c); hold on;
+    subplot(2,1,c); hold on;
     set(gca,'FontName','Calibri'); box off; set(gca,'Layer','top'); set(gca,'Fontsize',32);
     for g = size(smoothed_clusters{er,1},1):-1:1 % For each group
         legend_lines(1,g) = plot(lb_merge{er,1}(5):lb_merge{er,1}(7),...
             smoothed_clusters{er,c}(g,:),'color',cmap{set_token}(g,:),'linewidth',5);
-        num(g) = sum(i_group_tags(i_experiment_reps == er) == g);
+        %num(g) = sum(i_group_tags(i_experiment_reps == er) == g);
     end
     
     % Find the top & bottom
-    scrap(1,1) = 0.5;
-    scrap(2,1) = 0;
+    scrap(1,1) = 0.5; % hard coded 
+    scrap(2,1) = 0; % hard coded 
     
     % Night patches
     r = rectangle('Position',[lb_merge{er,1}(6) 0 (lb_merge{er,1}(7) - lb_merge{er,1}(6)) ... 
@@ -60,16 +59,18 @@ for c = 1:length(m) % for each module of interest
         (lb_merge{er,1}(7) - time_bins) ...
         scrap(2,1) scrap(1,1)]);
     box off; set(gca, 'Layer','top'); set(gca,'Fontsize',32);
-    xlabel('Time (Hours)','Fontsize',32);
-    set(gca,'XTick',[lb_merge{er,1}(6)]); 
-    set(gca,'XTickLabels',{'23:00'}); 
+    xlabel('Time','Fontsize',32);
+    set(gca,'XTick',[(lb_merge{er,1}(5)+ fps{set_token}*60*30)... 
+        (lb_merge{er,1}(6) - fps{set_token}*60*30)...
+        (lb_merge{er,1}(7) - fps{set_token}*60*150)]); 
+    set(gca,'XTickLabels',{'09:30','22:30','06:30'}); 
     ylabel('Mean Frequency','Fontsize',32);
     
     % legend
-    if c == length(m)
-        legend(legend_lines,{horzcat('CHD8^{+/+},',' n = ',num2str(num(1))),...
-            horzcat('CHD8^{-/+},',' n = ',num2str(num(2))),...
-            horzcat('CHD8^{-/-},',' n = ',num2str(num(3)))});
+    if c == 1
+        legend(legend_lines,{horzcat('\itchd8^{+/+}'),...
+            horzcat('\itchd8^{-/+}'),...
+            horzcat('\itchd8^{-/-}')});
         legend('Boxoff');
     end
     
